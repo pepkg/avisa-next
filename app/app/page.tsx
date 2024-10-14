3ccca7183167336d39f7b79e0f653e5637bdcfee
pep <joragues@gmail.com>
2024-10-14 20:01:59 +0200

    added tailwind base css

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,11 +1,24 @@
 export default function AvisaApp() {
   return (
-    <div>
-      <h1>Bienvenido a la app Avisa</h1>
-      <form>
-        <label htmlFor="login">Login:</label>
-        <input type="text" id="login" name="login" />
-      </form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+        <h1 className="text-2xl font-bold mb-4">Bienvenido a la app Avisa</h1>
+        <form>
+          <label
+            htmlFor="login"
+            className="block text-lg font-medium text-gray-700"
+          >
+            Login:
+          </label>
+          <input
+            type="text"
+            id="login"
+            name="login"
+            placeholder="Escribe tu usuario"
+            className="mt-2 p-2 border border-gray-300 rounded w-full"
+          />
+        </form>
+      </div>
     </div>
   );
 }
--- app/avisa-web/page.tsx
+++ app/avisa-web/page.tsx
@@ -1,13 +1,23 @@
 export default function AvisaWeb() {
   return (
-    <div>
-      <h1>Welcome to Avisa Web</h1>
-      <label htmlFor="country-select">A qué país vas:</label>
-      <select id="country-select">
-        <option value="eua">EUA</option>
-        <option value="españa">España</option>
-        <option value="francia">Francia</option>
-      </select>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+        <h1 className="text-2xl font-bold mb-4">Welcome to Avisa Web</h1>
+        <label
+          htmlFor="country-select"
+          className="block text-lg font-medium text-gray-700"
+        >
+          A qué país vas:
+        </label>
+        <select
+          id="country-select"
+          className="mt-2 p-2 border border-gray-300 rounded"
+        >
+          <option value="eua">EUA</option>
+          <option value="españa">España</option>
+          <option value="francia">Francia</option>
+        </select>
+      </div>
     </div>
   );
 }
