7466a4678b6c5082732fedabf352324b6cfa913e
pep <joragues@gmail.com>
2024-10-20 19:11:50 +0200

    add order sort by translations list

--- app/app/dashboard/translations/page.tsx
+++ app/app/dashboard/translations/page.tsx
@@ -11,40 +11,102 @@ interface Translation {
   translations: Record<string, string>;
 }
 
+interface Language {
+  iso: string;
+  names: Record<string, string>;
+}
+
 export default function Translations() {
   const [translations, setTranslations] = useState<Translation[]>([]);
+  const [filteredTranslations, setFilteredTranslations] = useState<
+    Translation[]
+  >([]);
+  const [languages, setLanguages] = useState<Language[]>([]); // Para almacenar los idiomas
   const { translations: translationLiterals } = useTranslations();
   const [selectedTranslations, setSelectedTranslations] = useState<string[]>(
     [],
-  ); // Para la selección múltiple
+  );
+  const [searchTerm, setSearchTerm] = useState("");
   const [showDeleteDialog, setShowDeleteDialog] = useState<string | null>(null);
-  const [showBatchDeleteDialog, setShowBatchDeleteDialog] = useState(false); // Para eliminar por lotes
+  const [showBatchDeleteDialog, setShowBatchDeleteDialog] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState("");
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+  const [sortBy, setSortBy] = useState<"label" | "translations">("label");
+  const [activeTab, setActiveTab] = useState<{ [key: string]: string }>({}); // Para almacenar el tab activo de cada fila
   const router = useRouter();
   const searchParams = useSearchParams();
 
+  // Cargar las traducciones y los idiomas
   useEffect(() => {
     fetch("/api/translations")
       .then((res) => res.json())
-      .then((data) => setTranslations(data));
+      .then((data) => {
+        setTranslations(data);
+        setFilteredTranslations(data); // Inicialmente muestra todas las traducciones
+      });
+
+    fetch("/api/languages")
+      .then((res) => res.json())
+      .then((data) => setLanguages(data)); // Cargar los idiomas disponibles
   }, []);
 
-  // Mostrar mensajes de éxito según el parámetro
   useEffect(() => {
     if (searchParams.get("edit_success") === "true") {
-      setShowSuccessMessage("Traducción editada con éxito.");
+      setShowSuccessMessage(
+        translationLiterals["translations_edit_success_message"] ||
+          "Traducción editada con éxito.",
+      );
     } else if (searchParams.get("add_success") === "true") {
-      setShowSuccessMessage("Traducción agregada con éxito.");
+      setShowSuccessMessage(
+        translationLiterals["translations_add_success_message"] ||
+          "Traducción agregada con éxito.",
+      );
     } else if (searchParams.get("success") === "true") {
-      setShowSuccessMessage("Traducción eliminada con éxito.");
+      setShowSuccessMessage(
+        translationLiterals["translations_del_success_message"] ||
+          "Traducción eliminada con éxito.",
+      );
     }
-  }, [searchParams]);
+  }, [searchParams, translationLiterals]);
 
   const handleDismissMessage = () => {
     setShowSuccessMessage("");
   };
 
-  // Manejar la selección individual de una traducción
+  const handleSort = (column: "label" | "translations") => {
+    const newOrder = sortOrder === "asc" ? "desc" : "asc";
+    setSortBy(column);
+    setSortOrder(newOrder);
+
+    const sortedTranslations = [...filteredTranslations].sort((a, b) => {
+      const aValue = column === "label" ? a.label : a.translations.en;
+      const bValue = column === "label" ? b.label : b.translations.en;
+
+      if (aValue < bValue) return newOrder === "asc" ? -1 : 1;
+      if (aValue > bValue) return newOrder === "asc" ? 1 : -1;
+      return 0;
+    });
+
+    setFilteredTranslations(sortedTranslations);
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.toLowerCase();
+    setSearchTerm(value);
+
+    const filtered = translations.filter((trans) =>
+      trans.label.toLowerCase().includes(value),
+    );
+    setFilteredTranslations(filtered);
+  };
+
+  const handleResetSearch = () => {
+    setSearchTerm("");
+    setFilteredTranslations(translations); // Restablecer a todas las traducciones
+    setSortBy("label"); // Restablecer ordenación
+    setSortOrder("asc");
+  };
+
   const handleSelectTranslation = (id: string) => {
     setSelectedTranslations((prevSelected) =>
       prevSelected.includes(id)
@@ -53,16 +115,14 @@ export default function Translations() {
     );
   };
 
-  // Seleccionar/deseleccionar todos
   const handleSelectAll = () => {
-    if (selectedTranslations.length === translations.length) {
-      setSelectedTranslations([]); // Deseleccionar todo
+    if (selectedTranslations.length === filteredTranslations.length) {
+      setSelectedTranslations([]);
     } else {
-      setSelectedTranslations(translations.map((trans) => trans._id)); // Seleccionar todo
+      setSelectedTranslations(filteredTranslations.map((trans) => trans._id));
     }
   };
 
-  // Eliminar traducciones seleccionadas por lotes
   const handleBatchDelete = async () => {
     if (selectedTranslations.length > 0) {
       const promises = selectedTranslations.map((id) =>
@@ -76,9 +136,17 @@ export default function Translations() {
           (trans) => !selectedTranslations.includes(trans._id),
         ),
       );
+      setFilteredTranslations(
+        filteredTranslations.filter(
+          (trans) => !selectedTranslations.includes(trans._id),
+        ),
+      );
       setSelectedTranslations([]);
       setShowBatchDeleteDialog(false);
-      setShowSuccessMessage("Traducciones eliminadas con éxito.");
+      setShowSuccessMessage(
+        translationLiterals["translations_delete_message"] ||
+          "Traducciones eliminadas con éxito.",
+      );
     }
   };
 
@@ -89,21 +157,34 @@ export default function Translations() {
 
     if (res.ok) {
       setTranslations(translations.filter((trans) => trans._id !== id));
+      setFilteredTranslations(
+        filteredTranslations.filter((trans) => trans._id !== id),
+      );
       setShowDeleteDialog(null);
       router.push("/app/dashboard/translations?success=true");
     } else {
-      console.error("Failed to delete translation");
+      console.error(
+        translationLiterals["translations_delete_err_message"] ||
+          "Failed to delete translation",
+      );
     }
+  };
+
+  // Manejar el cambio de tab
+  const handleTabChange = (id: string, iso: string) => {
+    setActiveTab((prevTabs) => ({
+      ...prevTabs,
+      [id]: iso,
+    }));
   };
 
   return (
     <DashboardLayout>
-      {/* Mostrar mensaje de éxito */}
       {showSuccessMessage && (
         <div className="bg-green-100 text-green-800 p-4 rounded mb-4">
           <p>{showSuccessMessage}</p>
           <button className="text-red-500 ml-4" onClick={handleDismissMessage}>
-            Cerrar
+            {translationLiterals["close_button"] || "Cerrar"}
           </button>
         </div>
       )}
@@ -113,6 +194,30 @@ export default function Translations() {
           "Gestionar Traducciones"}
       </h1>
 
+      <div className="mb-4 flex items-center justify-between">
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            placeholder={
+              translationLiterals["search_placeholder"] || "Buscar por literal"
+            }
+            className="input input-bordered w-full mr-2"
+          />
+          <button className="btn btn-secondary" onClick={handleResetSearch}>
+            {translationLiterals["reset_button"] || "Resetear"}
+          </button>
+        </div>
+        <a
+          href="/app/dashboard/translations/edit/new"
+          className="btn btn-primary ml-4"
+        >
+          {translationLiterals["add_translation_button"] ||
+            "Agregar nueva traducción"}
+        </a>
+      </div>
+
       <table className="min-w-full border-collapse">
         <thead>
           <tr>
@@ -120,14 +225,22 @@ export default function Translations() {
               <input
                 type="checkbox"
                 onChange={handleSelectAll}
-                checked={selectedTranslations.length === translations.length}
+                checked={
+                  selectedTranslations.length === filteredTranslations.length
+                }
               />
             </th>
             <th className="border px-4 py-2">
               {translationLiterals["label_column"] || "Etiqueta"}
+              <button onClick={() => handleSort("label")}>
+                {sortBy === "label" && sortOrder === "asc" ? "▲" : "▼"}
+              </button>
             </th>
             <th className="border px-4 py-2">
               {translationLiterals["translations_column"] || "Traducciones"}
+              <button onClick={() => handleSort("translations")}>
+                {sortBy === "translations" && sortOrder === "asc" ? "▲" : "▼"}
+              </button>
             </th>
             <th className="border px-4 py-2">
               {translationLiterals["actions_column"] || "Acciones"}
@@ -135,7 +248,7 @@ export default function Translations() {
           </tr>
         </thead>
         <tbody>
-          {translations.map((trans) => (
+          {filteredTranslations.map((trans) => (
             <tr key={trans._id} className="border-t">
               <td className="border px-4 py-2">
                 <input
@@ -146,7 +259,23 @@ export default function Translations() {
               </td>
               <td className="border px-4 py-2">{trans.label}</td>
               <td className="border px-4 py-2">
-                {trans.translations.en} / {trans.translations.es}
+                <div className="tabs">
+                  {languages.map((lang) => (
+                    <button
+                      key={lang.iso}
+                      className={`tab ${
+                        activeTab[trans._id] === lang.iso ? "tab-active" : ""
+                      }`}
+                      onClick={() => handleTabChange(trans._id, lang.iso)}
+                    >
+                      {lang.iso.toUpperCase()}
+                    </button>
+                  ))}
+                </div>
+                <div className="translation-content">
+                  {trans.translations[activeTab[trans._id]] ||
+                    trans.translations.en}
+                </div>
               </td>
               <td className="border px-4 py-2">
                 <a
@@ -167,7 +296,6 @@ export default function Translations() {
         </tbody>
       </table>
 
-      {/* Botón de eliminar por lotes */}
       {selectedTranslations.length > 0 && (
         <button
           className="btn btn-danger mt-4"
@@ -178,7 +306,6 @@ export default function Translations() {
         </button>
       )}
 
-      {/* Diálogo de confirmación para eliminar por lotes */}
       {showBatchDeleteDialog && (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
           <div className="bg-white p-6 rounded shadow-lg">
